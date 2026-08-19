2242a88f0456405e4ee68dff673d4e9df92b39d3
kamil-tokarz <kamil.tokarz@gmail.com>
2026-08-19 13:24:36 +0200

    Refine survey UX and question flow

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.JagoProFeedbackConfig={storageMode:'local',submitEndpoint:'',analyticsEndpoint:'',surveyVersion:'2026.1'};
+window.JagoProFeedbackConfig={storageMode:'local',submitEndpoint:'',analyticsEndpoint:'',surveyVersion:'2026.2'};
