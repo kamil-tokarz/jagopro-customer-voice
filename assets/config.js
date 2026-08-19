dff35972074e53f9555a1b24202be2b5d312b71e
kamil-tokarz <kamil.tokarz@gmail.com>
2026-08-19 13:43:53 +0200

    Refine PL and EN rating matrix

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.JagoProFeedbackConfig={storageMode:'local',submitEndpoint:'',analyticsEndpoint:'',surveyVersion:'2026.4'};
+window.JagoProFeedbackConfig={storageMode:'local',submitEndpoint:'',analyticsEndpoint:'',surveyVersion:'2026.5'};
