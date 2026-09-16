68419c69866d4977759a5efb743237e09e48668a
kamil-tokarz <kamil.tokarz@gmail.com>
2026-09-16 08:27:12 +0200

    Set survey schema version to 2.1

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.JagoProFeedbackConfig={storageMode:'local',submitEndpoint:'',analyticsEndpoint:'',surveyVersion:'2.0'};
+window.JagoProFeedbackConfig={storageMode:'local',submitEndpoint:'',analyticsEndpoint:'',surveyVersion:'2.1'};
